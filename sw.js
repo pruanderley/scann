6cb76494d037855f00c48ca09837132b1a857428
pruanderley <108106947+pruanderley@users.noreply.github.com>
2026-07-29 16:54:14 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,37 +1,26 @@
 const CACHE_NAME = 'puzzle-scanner-v1';
 const urlsToCache = [
-  '/',
-  '/index.html',
-  '/manifest.json',
-  '/icon-192x192.png',
-  '/icon-512x512.png'
+  '/puzzle71-scanner/',
+  '/puzzle71-scanner/index.html',
+  '/puzzle71-scanner/manifest.json',
+  '/puzzle71-scanner/worker.js',
+  '/puzzle71-scanner/icon-192x192.png',
+  '/puzzle71-scanner/icon-512x512.png'
 ];
 
 self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
   );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(clients.claim());
 });
 
 self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request)
-      .then(response => response || fetch(event.request))
-  );
-});
-
-self.addEventListener('activate', event => {
-  const cacheWhitelist = [CACHE_NAME];
-  event.waitUntil(
-    caches.keys().then(cacheNames => {
-      return Promise.all(
-        cacheNames.map(cacheName => {
-          if (!cacheWhitelist.includes(cacheName)) {
-            return caches.delete(cacheName);
-          }
-        })
-      );
-    })
+    caches.match(event.request).then(response => response || fetch(event.request))
   );
 });
